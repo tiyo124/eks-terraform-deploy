229a74e1bb97cd25040a4a06e0c87fe79933413f
tayo <tiyo124@yahoo.com>
2025-03-29 23:58:32 -0400

    update

--- dev/backend.tf
+++ dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "dev/terraform.state"
-    bucket         = "class38dominion-terraform-backend"
+    bucket         = "tayoclass38dominion-terraform-backend"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }
--- uat/backend.tf
+++ uat/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "prod/terraform.state"
-    bucket         = "class38dominion-terraform-backend"
+    bucket         = "tayoclass38dominion-terraform-backend"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
   }

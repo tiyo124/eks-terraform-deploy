c95e6e5905806843d0500dee4d10710f6ec9cd71
tayo <tiyo124@yahoo.com>
2025-04-02 01:59:50 -0400

    env update

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key            = "production/terraform.state"
+    key            = "prod/terraform.state"
     bucket         = "tayoclass38dominion-terraform-backend"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
